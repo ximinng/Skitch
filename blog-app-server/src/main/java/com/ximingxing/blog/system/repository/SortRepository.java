bf7e66c3a308fe699ef5932e25bddb77f9edaa69
Ximing Xing <ximingxing@gmail.com>
2020-01-31 00:33:14 +0800

    blog-app-server: Sort domin access method done

--- blog-app-server/src/main/java/com/ximingxing/blog/system/repository/SortRepository.java
+++ blog-app-server/src/main/java/com/ximingxing/blog/system/repository/SortRepository.java
@@ -4,10 +4,28 @@ import com.ximingxing.blog.system.entity.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Description: LabelRepository
  * Created By xxm
  */
 @Repository
 public interface SortRepository extends JpaRepository<Sort, Integer> {
+
+    /**
+     * 通过分类Id查询分类项
+     *
+     * @param sortId
+     * @return
+     */
+    Optional<Sort> findSortBySortId(Integer sortId);
+
+    /**
+     * 通过分类名称查询分类项
+     *
+     * @param sortName
+     * @return
+     */
+    Optional<Sort> findSortBySortName(String sortName);
 }
--- blog-app-server/src/test/java/com/ximingxing/blog/system/repository/SortRepositoryTest.java
+++ blog-app-server/src/test/java/com/ximingxing/blog/system/repository/SortRepositoryTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,9 +25,42 @@ public class SortRepositoryTest {
 
     @Test
     public void saveSortMethodTest() {
-        Sort sort = sortRepository.save(new Sort(null, "分类One", null));
+        Sort sort1 = sortRepository.save(new Sort(1, "分类One", null));
+        System.out.println(sort1.getSortId());
+
+        Sort sort2 = sortRepository.save(new Sort(2, "分类Two", null));
+        System.out.println(sort2.getSortId());
+
+        Sort sort3 = sortRepository.save(new Sort(3, "分类Three", null));
+        System.out.println(sort3.getSortId());
+        sort3.setParentId(sort3.getSortId()); // 顶层分类: sort_id == parent_id
+        sortRepository.save(sort3);
+
+        // 1 <- 2 <- 3
+        sort1.setParentId(sort2.getSortId());
+        sort2.setParentId(sort3.getSortId());
+        sortRepository.save(sort1);
+        sortRepository.save(sort2);
+    }
+
+    @Test
+    public void findSortRecursionMethodTest() {
+        // 给定分类名称查询父分类
+        String tar = "分类One";
+        Optional<Sort> sortOption = sortRepository.findSortBySortName(tar);
+        Integer sortId = sortOption.get().getSortId();
+
+        // 递归查询父分类
+        while (sortId != null) {
+            Optional<Sort> sortBySortId = sortRepository.findSortBySortId(sortId);
+        }
+    }
+
+    @Test
+    public void saveTopSortMethodTest() {
+        Sort sort = sortRepository.save(new Sort(-1, "分类Three", null));
         System.out.println(sort.getSortId());
-//        sort.setParentId(sort.getSortId()); // 顶层分类
-//        sortRepository.save(sort);
+        sort.setParentId(sort.getSortId()); // 顶层分类: sort_id == parent_id
+        sortRepository.save(sort);
     }
 }
